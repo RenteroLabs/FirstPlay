784f0a05348857dd9fcd65a5350cde7ce1d0ea13
D-lyw <liuyuanwang4321@gmail.com>
2023-01-09 10:56:43 +0800

    feat: add show more button in mobile

--- components/PageHome/HotGames/index.tsx
+++ components/PageHome/HotGames/index.tsx
@@ -1,8 +1,11 @@
 import GameCard from "@/components/GameCard"
-import { Box } from "@mui/material"
+import { Box, useMediaQuery } from "@mui/material"
 import Link from "next/link"
+import { useState } from "react"
 import SectionTitle from "../components/SectionTitle"
 import styles from './style.module.scss'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 interface HotGamesProps {
   hotGames: Record<string, any>[]
@@ -11,16 +14,31 @@ interface HotGamesProps {
 
 const HotGames: React.FC<HotGamesProps> = (props) => {
   const { hotGames, timestamp } = props
-  
+
+  const is600Size = useMediaQuery("(max-width: 600px)")
+
+  const [showMore, setShowMore] = useState<boolean>(false)
+
   return <Box className={styles.hotGames}>
     <Box className={styles.hotGamesBox}>
       <SectionTitle normal="Games" emphasize="Hot" />
       <Box className={styles.cardList}>
         {
-         hotGames.map((item, index) => <GameCard gameInfo={item} key={index} timestamp={timestamp} />)
+          ((is600Size && !showMore) ? hotGames.slice(0, 6) : hotGames).map((item, index) => <GameCard gameInfo={item} key={index} timestamp={timestamp} />)
+        }
+        {
+          // hotGames.map((item, index) => <GameCard gameInfo={item} key={index} timestamp={timestamp} />)
         }
       </Box>
     </Box>
+
+    {is600Size && <Box className={styles.showMoreBtn} onClick={() => setShowMore(!showMore)}>
+      {
+        showMore ?
+          <> Less <KeyboardArrowUpIcon /> </> :
+          <> More <KeyboardArrowDownIcon /> </>
+      }
+    </Box>}
   </Box>
 }
 
--- components/PageHome/RewardGames/index.tsx
+++ components/PageHome/RewardGames/index.tsx
@@ -1,8 +1,11 @@
 import RewardGameCard from "@/components/RewardGameCard"
-import { Box } from "@mui/material"
+import { Box, useMediaQuery } from "@mui/material"
 import Link from "next/link"
 import SectionTitle from "../components/SectionTitle"
 import styles from './styles.module.scss'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { useState } from "react"
 
 interface RewardGamesProps {
   rewardGames: Record<string, any>[]
@@ -12,15 +15,30 @@ interface RewardGamesProps {
 const RewardGames: React.FC<RewardGamesProps> = (props) => {
   const { rewardGames, timestamp } = props
 
+  const is600Size = useMediaQuery("(max-width: 600px)")
+
+  const [showMore, setShowMore] = useState<boolean>(false)
+
   return <Box className={styles.rewardGames}>
     <Box className={styles.rewardGamesBox}>
       <SectionTitle normal="" emphasize="Rewarded Games" />
       <Box className={styles.cardList}>
         {
-          rewardGames.map((item, index) => <RewardGameCard gameInfo={item} key={index} timestamp={timestamp} />)
+          ((is600Size && !showMore) ? rewardGames.slice(0, 3) : rewardGames).map((item, index) => <RewardGameCard gameInfo={item} key={index} timestamp={timestamp} />)
+        }
+        {
+          // rewardGames.map((item, index) => <RewardGameCard gameInfo={item} key={index} timestamp={timestamp} />)
         }
       </Box>
     </Box>
+
+    {is600Size && <Box className={styles.showMoreBtn} onClick={() => setShowMore(!showMore)}>
+      {
+        showMore ?
+          <> Less <KeyboardArrowUpIcon /> </> :
+          <> More <KeyboardArrowDownIcon /> </>
+      }
+    </Box>}
   </Box>
 }
 
