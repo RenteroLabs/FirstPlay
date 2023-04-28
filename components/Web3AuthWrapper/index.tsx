1c234bd874a2fb9b5d1d4c1a21171968fd6f2f1a
D-lyw <liuyuanwang4321@gmail.com>
2023-04-28 16:14:58 +0800

    fix: JSON.parse localstorage item error

--- components/Web3AuthWrapper/index.tsx
+++ components/Web3AuthWrapper/index.tsx
@@ -22,13 +22,19 @@ export const Web3AuthWrapper: React.FC<React.PropsWithChildren> = ({ children })
     const walletType = localStorage.getItem("wagmi.wallet");
     const loginType = localStorage.getItem("openlogin_store") as string;
 
-    console.log(wagmiConnected, walletType, JSON.parse(loginType).typeOfLogin)
+    let web3authType: string | null = null
+    try {
+      web3authType = JSON.parse(loginType)?.typeOfLogin
+    } catch (err) {
+      console.log(err)
+    }
+    console.log(wagmiConnected, walletType, web3authType)
 
     // 如果是 Web3AuthConnector 连接，页面刷新后，需要重新手动连接调用，wagmi 的 autoConnect 无效
     if (walletType === '"web3auth"') {
       if (wagmiConnected === 'true') {
         console.log("connect")
-        switch (JSON.parse(loginType).typeOfLogin) {
+        switch (web3authType) {
           case 'google': connect({ connector: GoogleConnector }); break;
           case 'facebook': connect({ connector: FacebookConnector }); break;
           case 'twitter': connect({ connector: TwitterConnector }); break;
